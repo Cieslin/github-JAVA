767eb71a4c0e799ad51050242d6b37c5acfb64d9
Jakub Cieslinski <jakub.cieslinski02@gmail.com>
2018-02-06 03:07:34 +0100

    Added new Spring Project: Hello World!

---
+++ hello_world_spring/src/main/java/com/cieslin/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.cieslin.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World! I'm Cieslin";
+    }
+}
